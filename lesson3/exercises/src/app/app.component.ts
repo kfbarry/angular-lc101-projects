8fe2478894c0511607a1552fe9405868f8a51e23
Kieran <kieranbarry51@gmail.com>
2022-03-23 22:40:12 -0500

    I tried a bunch of stuff to get the rocket to move using the prompeted method, it never worked so I eventually tried to just copy and paste the solution and that also didn't work

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { buildDriverProvider } from 'protractor/built/driverProviders';
+import { parse } from 'querystring';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +14,35 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+ 
+
+  takeOff(){
+    window.confirm("Confirm that the shuttle is ready for takeoff.");
+    this.message = "Shuttle in flight.";
+    this.color = "blue"
+    this.height += 10000;
+  }
+
+  land(){
+    window.alert("The shuttle is landing. Landing gear engaged.");
+    this.message ="The shuttle has landed.";
+    this.color = "green";
+    this.height =0;
+  }
+
+  abort(){
+    window.confirm("Do you really want to abort the mission? do you really really? I mean really really?");
+    this.message ="Mission aborted.";
+    this.color = "red";
+    this.height =0;
+  }
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+       let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+       rocketImage.style.left = movement;
+       this.width = this.width + 10000;
+    }
+ }
+
 }
